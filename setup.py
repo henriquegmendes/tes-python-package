0763fdb7fd5b2d5eacddc3f3eaba5c6620a8c434
Henrique Mendes <henrique.gmendes@hotmail.com>
2024-03-10 00:56:45 -0300

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hentestpackage',
+    version="0.0.1",
+    packages=find_packages(),
+    description='Test',
+    include_package_data=True,
+    url='https://github.com/henriquegmendes/tes-python-package/',
+)
